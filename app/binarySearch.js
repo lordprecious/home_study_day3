86ec5edfa81ae43ecdf8e64265ead075a46e0726
lordprecious <lordprecious1@gmail.com>
2017-03-16 12:43:26 +0100

    fixed a bug i created while editing my comments

--- app/binarySearch.js
+++ app/binarySearch.js
@@ -2,35 +2,33 @@
 
 var file = {}
 
-// function to generate an array containing 1 to 20;
-Array.prototype.toTwenty = function () { 
+Array.prototype.toTwenty = function() { // function to generate an array containing 1 to 20;
   var oneToTwenty = [];
-  for (var i = 1; i <= 20; i++) {
+  for(var i = 1; i <= 20; i++) {
     oneToTwenty.push(i);
   }
   return oneToTwenty;
 };
 
-// function to generate an array containing 2 to 40 with an icreament of +2;
-Array.prototype.toForty = function () {  
+
+Array.prototype.toForty = function() {  // function to generate an array containing 2 to 40 with an icreament of +2;
   var twoToForty = [];
-  for (var i = 2; i <= 40; i += 2) {
+  for(var i = 2; i <= 40; i += 2) {
     twoToForty.push(i);
   }
   return twoToForty;
 };
 
-// function to generate an array containing 10 to 1000 with an increament of +10
-Array.prototype.toOneThousand = function() {  
+
+Array.prototype.toOneThousand = function() {  // function to generate an array containing 10 to 1000 with an increament of +10
   var tenToOneThousand = [];
-  for (var i = 10; i <= 1000; i += 10) {
+  for(var i = 10; i <= 1000; i += 10) {
     tenToOneThousand.push(i);
   }
   return tenToOneThousand;
 };
 
-// the binary search function;
-Array.prototype.search = function (num) {   
+Array.prototype.search = function(num) {   // the binary search function;
   var minIndex = 0;
   var maxIndex = this.length  -1;
   var currentElement;
@@ -41,30 +39,26 @@ Array.prototype.search = function (num) {
 
   while(minIndex <= maxIndex) {
 
-    // getting the mid point of the array
-    currentIndex = Math.floor((minIndex + maxIndex) / 2 );   
+    currentIndex = Math.floor((minIndex + maxIndex) / 2 );   // getting the mid point of the array
     currentElement = this[currentIndex];
 
-     // check if middle element equals the search element
-    if (this[minIndex] === num) {     
+    if (this[minIndex] === num) {      // check if middle element equals the search element
       
       this.index = minIndex;
       break;
-
-    // check if the last element in the array equals the search element
-    } else if (this[maxIndex] === num) {  
+    }
+    else if (this[maxIndex] === num) {   //check if the last element in the array equals the search element
       this.index = maxIndex;
       break;
     }
 
-    // condition that checks if the current element is less than the search element;
-    if (currentElement < num) {         
+    if (currentElement < num) {          // condition that checks if the current element is less than the search element;
       minIndex = currentIndex + 1;
-
-    } else if (currentElement > num) {
+    }
+    else if (currentElement > num) {
       maxIndex = currentIndex - 1;
 
-    }s else {
+    } else {
       this.index = currentIndex;
       break;
     }
