1347373ed2041e09d09aab2f94c987aa15326944
lordprecious <lordprecious1@gmail.com>
2017-03-16 12:53:15 +0100

    commenting

--- app/binarySearch.js
+++ app/binarySearch.js
@@ -2,7 +2,8 @@
 
 var file = {}
 
-Array.prototype.toTwenty = function() { // function to generate an array containing 1 to 20;
+// function to generate an array containing 1 to 20;
+Array.prototype.toTwenty = function () { 
   var oneToTwenty = [];
   for(var i = 1; i <= 20; i++) {
     oneToTwenty.push(i);
@@ -11,7 +12,8 @@ Array.prototype.toTwenty = function() { // function to generate an array contain
 };
 
 
-Array.prototype.toForty = function() {  // function to generate an array containing 2 to 40 with an icreament of +2;
+// function to generate an array containing 2 to 40 with an icreament of +2;
+Array.prototype.toForty = function() {  
   var twoToForty = [];
   for(var i = 2; i <= 40; i += 2) {
     twoToForty.push(i);
@@ -20,7 +22,8 @@ Array.prototype.toForty = function() {  // function to generate an array contain
 };
 
 
-Array.prototype.toOneThousand = function() {  // function to generate an array containing 10 to 1000 with an increament of +10
+// function to generate an array containing 10 to 1000 with an increament of +10
+Array.prototype.toOneThousand = function() {  
   var tenToOneThousand = [];
   for(var i = 10; i <= 1000; i += 10) {
     tenToOneThousand.push(i);
@@ -28,7 +31,9 @@ Array.prototype.toOneThousand = function() {  // function to generate an array c
   return tenToOneThousand;
 };
 
-Array.prototype.search = function(num) {   // the binary search function;
+
+// the binary search function;
+Array.prototype.search = function (num) {   
   var minIndex = 0;
   var maxIndex = this.length  -1;
   var currentElement;
@@ -37,25 +42,29 @@ Array.prototype.search = function(num) {   // the binary search function;
   this.count = 0;
   this.index = -1;
 
-  while(minIndex <= maxIndex) {
+  while (minIndex <= maxIndex) {
 
-    currentIndex = Math.floor((minIndex + maxIndex) / 2 );   // getting the mid point of the array
+    // getting the mid point of the array
+    currentIndex = Math.floor((minIndex + maxIndex) / 2 );  
     currentElement = this[currentIndex];
 
-    if (this[minIndex] === num) {      // check if middle element equals the search element
+    // check if middle element equals the search element
+    if (this[minIndex] === num) {      
       
       this.index = minIndex;
       break;
-    }
-    else if (this[maxIndex] === num) {   //check if the last element in the array equals the search element
+
+    //check if the last element in the array equals the search element  
+    } else if (this[maxIndex] === num) {   
       this.index = maxIndex;
       break;
     }
 
-    if (currentElement < num) {          // condition that checks if the current element is less than the search element;
+    // condition that checks if the current element is less than the search element;
+    if (currentElement < num) {          
       minIndex = currentIndex + 1;
-    }
-    else if (currentElement > num) {
+
+    } else if (currentElement > num) {
       maxIndex = currentIndex - 1;
 
     } else {
