94911a3eec4a8314e93e70fe246a14d4178f9776
lordprecious <lordprecious1@gmail.com>
2017-03-16 12:05:36 +0100

    made corrections on commenting my code

--- app/findMissing.js
+++ app/findMissing.js
@@ -2,27 +2,35 @@
 
 var file = {};
 
-/** a function called findMissing(arr1, arr2)  that takes in two arguments, both of which are arrays. One of the arrays will have one extra number, the function returns the odd value; */
+/** a function called findMissing(arr1, arr2)  that takes in two arguments,
+both of which are arrays. One of the arrays will have one extra number, 
+the function returns the odd value; */
+
 file.findMissing = function (arr1, arr2) {
 
-	if(arr1.length === 0 || arr2.length === 0) { // condition that checks if any of the arrray is empty;
+	// condition that checks if any of the arrray is empty;
+	if(arr1.length === 0 || arr2.length === 0) { 
 		return 0;
 
-	} else if (arr1.length === arr2.length) {    // condition that checks if the length of both arrays are the same size then returns 0 if true
+	// condition that checks if the length of both arrays are the same size then returns 0 if true
+	} else if (arr1.length === arr2.length) {    
 		return 0;
 
-	}  else if(arr1.length !== arr2.length) {	 // condition that checks if the length of both arrays are diffrent
+	// condition that checks if the length of both arrays are diffrent
+	} else if(arr1.length !== arr2.length) {	 
 		var missingNumber = [];
-		var joined = arr1.concat(arr2);			 // concantenate both arrays
+		// concantenate both arrays
+		var joined = arr1.concat(arr2);			 
 		for(var i = 0; i < joined.length; i++ ) {
         var current = joined[i];
 
-        if( joined.indexOf(current) == joined.lastIndexOf(current) ) {		// condition that checks if there are reapeated elements in the  concantenated array;
+        // condition that checks if there are reapeated elements in the  concantenated array;
+        if( joined.indexOf(current) == joined.lastIndexOf(current) ) {		
           missingNumber.push(current);
         }
       }
-
-      return missingNumber[0];		// returns the element that is not repeated;
+      // returns the element that is not repeated;
+      return missingNumber[0];		
 	}
 }
 
